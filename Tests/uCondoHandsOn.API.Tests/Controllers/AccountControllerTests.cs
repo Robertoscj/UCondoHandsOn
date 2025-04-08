222434b19ce497cfeb1bedcf22771c3a4ee5f41e
roberto carlos <robertomscjp@gmail.com>
2025-04-07 22:40:33 -0300

    uCondoHands - Tests

--- Tests/uCondoHandsOn.API.Tests/Controllers/AccountControllerTests.cs
+++ Tests/uCondoHandsOn.API.Tests/Controllers/AccountControllerTests.cs
@@ -44,7 +44,101 @@ namespace uCondoHandsOn.API.Tests.Controllers
             Assert.IsType<OkResult>(result);
         }
 
+
+
+      [Fact]
+        public async Task CreatedResult()
+        {
+            var mock = new Mock<IAccountsService>();
+
+            mock.Setup(x => x.CreateAsync(It.IsAny<AccountCreateDto>()))
+                .ReturnsAsync(() => ValidationResult<AccountDto>.Success(null));
+
+            _controller = new AccountsController(mock.Object);
+
+            var result = await _controller.CreateAsync(new AccountCreateDto
+            {
+                Code = "1",
+                Name = "Tests"
+            });
+
+            Assert.IsType<CreatedResult>(result);
+        }
+
+
+
+        [Fact]
+        public async Task Create_InvalidFormatBadRequest()
+        {
+            var mock = new Mock<IAccountsService>();
+
+            mock.Setup(x => x.CreateAsync(It.IsAny<AccountCreateDto>()))
+                .ReturnsAsync(() => ValidationResult<AccountDto>.Success(null));
+
+            _controller = new AccountsController(mock.Object);
+
+            var result = await _controller.CreateAsync(new AccountCreateDto
+            {
+                Code = "1..0",
+                Name = "Test"
+            });
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Create_WithAlphaCodeReturnsBadRequest()
+        {
+            var mock = new Mock<IAccountsService>();
+
+            mock.Setup(x => x.CreateAsync(It.IsAny<AccountCreateDto>()))
+                .ReturnsAsync(() => ValidationResult<AccountDto>.Success(null));
+
+            _controller = new AccountsController(mock.Object);
+
+            var result = await _controller.CreateAsync(new AccountCreateDto
+            {
+                Code = "a.b.c.d",
+                Name = "Test"
+            });
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
         
+
+        [Fact]
+        public async Task Create_EmptyNameReturnsBadRequest()
+        {
+            var mock = new Mock<IAccountsService>();
+
+            mock.Setup(x => x.CreateAsync(It.IsAny<AccountCreateDto>()))
+                .ReturnsAsync(() => ValidationResult<AccountDto>.Success(null));
+
+            _controller = new AccountsController(mock.Object);
+
+            var result = await _controller.CreateAsync(new AccountCreateDto
+            {
+                Code = "1",
+                Name = string.Empty
+            });
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task Delete_ValidIdReturnsNoContent()
+        {
+            var mock = new Mock<IAccountsService>();
+
+            mock.Setup(x => x.DeleteAsync(It.IsAny<string>()))
+                .ReturnsAsync(() => ValidationResult.Success());
+
+            _controller = new AccountsController(mock.Object);
+
+            var result = await _controller.DeleteAsync("1");
+
+            Assert.IsType<NoContentResult>(result);
+        }
 
     }
 }
--- Tests/uCondoHandsOn.Application.Tests/AccountServiceTests.cs
+++ Tests/uCondoHandsOn.Application.Tests/AccountServiceTests.cs
@@ -9,11 +9,38 @@ namespace uCondoHandsOn.Application.Tests
     {
         IAccoutntService _accountService;
 
-        private readonly IAccount _accountParent = new Account
-        {
-           Code = "1",
-           Name = "Incomes",
-           Type = AccountType.Income,
-        }
+    private readonly IAccount _accountParent = new Account
+    {
+        Code = "1",
+        Name = "Incomes",
+        Type = AccountType.Income,
+    };
+
+    private readonly AccountEntity _RootParent = new AccountEntity
+    {
+        Code = "2",
+        Name = "Expenses",
+        Type = AccountType.Expense
+    };
+
+
+    private readonly AccountEntity _incomeChild = new AccountEntity
+    {
+        Code = "1.2",
+        ParentCode = "1",
+        Name = "Incomes Child",
+        Type = AccountType.Income
+    };
+
+    private readonly AccountEntity _incomeChilds = new AccountEntity
+    {
+        Code = "1.10",
+        ParentCode = "1",
+        Name = "Incomes Childs 10",
+        Type = AccountType.Income
+ };
+
+
+
     }
 }
